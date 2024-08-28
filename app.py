564e696f93c3379b2cdf07910e468c5c7e094e3d
Nekitori17 <cuongnguyen286641@gmail.com>
2024-08-28 15:29:41 +0700

    remove(): threading

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, Response
-from threading import Thread
 import os
 import json
 import importlib
@@ -36,10 +35,5 @@ def main():
     request_payload,
   )
 
-def run():
+if __name__ == "__main__":
   app.run(host="0.0.0.0", port=8080, debug=True)
-
-def keep_alive():
-  t = Thread(target=run)
-  t.start()
-keep_alive()
